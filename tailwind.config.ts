e127c2b2ce15e671dadfc1b0bdb62bbf848f0273
justkelvin <muia.muinde@s.karu.ac.ke>
2025-02-05 05:48:01 +0300

    Enhance Tailwind CSS configuration with typography plugin and custom code styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,38 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       background: "var(--background)",
+  //       foreground: "var(--foreground)",
+  //     },
+  //   },
+  // },
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            code: {
+              backgroundColor: '#1f2937',
+              padding: '0.2em 0.4em',
+              borderRadius: '0.25rem',
+              fontWeight: '400',
+            },
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 } satisfies Config;
